7397e9e6af12c049529f6022de5ccab276d29f9a
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2015-11-19 17:46:21 +0100

    can be imported into agent process

--- wishful_ath9k_driver/ath9k_driver.py
+++ wishful_ath9k_driver/ath9k_driver.py
@@ -12,20 +12,21 @@ __version__ = "0.1.0"
 __email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
 
 class Ath9kDriver(object):
-    def __init__(self, agentPort):
+    def __init__(self, agentPort=None):
         self.log = logging.getLogger("{module}.{name}".format(
             module=self.__class__.__module__, name=self.__class__.__name__))
 
-        assert agentPort
-        self.port = agentPort
-        self.log.debug("Connect to Agent on port: {0}".format(agentPort))
-
         self.interfaces = None
 
-        #Connect to WiSHFUL Agent
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PAIR)
-        self.socket.connect("tcp://localhost:%s" % agentPort)
+        if agentPort:
+            self.port = agentPort
+            self.log.debug("Connect to Agent on port: {0}".format(agentPort))
+
+            #Connect to WiSHFUL Agent
+            self.context = zmq.Context()
+            self.socket = self.context.socket(zmq.PAIR)
+            self.socket.connect("tcp://localhost:%s" % agentPort)
+
 
     def set_interfaces(self, interfaces):
         self.interfaces = interfaces
@@ -43,7 +44,24 @@ class Ath9kDriver(object):
         
         return  response
 
-    def process_msgs(self, socket):
+    def process_msgs(self, msgContainer):
+        assert len(msgContainer) == 3
+        group = msgContainer[0]
+        msgDesc = msgMgmt.MsgDesc()
+        msgDesc.ParseFromString(msgContainer[1])
+        msg = msgContainer[2]
+
+        self.log.debug("ATH9k process msg: {0}::{1}".format(msgDesc.msg_type, msg))
+
+        command = msg
+
+        response = None
+        if msgDesc.msg_type == "RADIO" and command == "SET_CHANNEL":
+            response = self.set_channel(1)
+
+        return response
+
+    def start_receive_msgs(self, socket):
          while True:
                 msgContainer = socket.recv_multipart()
 
@@ -54,12 +72,8 @@ class Ath9kDriver(object):
                 msg = msgContainer[2]
 
                 self.log.debug("ATH9K driver recived msg: {0}::{1}::{2}".format(group, msgDesc.msg_type, msg))
-                self.log.debug("ATH9k process msg: {0}::{1}::{2}".format(group, msgDesc.msg_type, msg))
 
-                response = None
-                command = msg
-                if msgDesc.msg_type == "RADIO" and command == "SET_CHANNEL":
-                    response = self.set_channel(1)
+                response = self.process_msgs(msgContainer)
 
                 if response:
                     self.log.debug("ATH9k sends response: {0}".format(response))
@@ -69,6 +83,6 @@ class Ath9kDriver(object):
     def run(self):
         self.log.debug("ath9k_driver starts".format())
         try:
-            self.process_msgs(self.socket)
+            self.start_receive_msgs(self.socket)
         except KeyboardInterrupt:
             self.log.debug("ATH9k_driver exits")
