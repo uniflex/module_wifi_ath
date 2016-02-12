57ba4a55ea5546551c8589788aaa6de8d0c90a80
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-12 15:46:22 +0100

    improved callbacks

--- wishful_ath9k_driver/ath9k_driver.py
+++ wishful_ath9k_driver/ath9k_driver.py
@@ -1,9 +1,7 @@
 import logging
-import zmq
-import random
-import sys
-import time
 import wishful_module
+import wishful_upis as upis
+
 
 __author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
 __copyright__ = "Copyright (c) 2015, Technische Universität Berlin"
@@ -11,15 +9,15 @@ __version__ = "0.1.0"
 __email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
 
 
-@wishful_module.decorate_module_class
+@wishful_module.build_module
 class Ath9kDriver(wishful_module.WishfulModule):
     def __init__(self, agentPort=None):
         super(Ath9kDriver, self).__init__(agentPort)
-        self.log = logging.getLogger('ath9k_driver.main')
+        self.log = logging.getLogger('ath9k_module.main')
         self.interface = "wlan0"
 
-    @wishful_module.add_msg_callback('set_channel')
-    def set_channel_test(self, channel):
+    @wishful_module.bind_function(upis.radio.set_channel)
+    def set_channel(self, channel):
         self.log.debug("ATH9K sets channel: {} on interface: {}".format(channel, self.interface))
 
         return "SET_CHANNEL_OK"
