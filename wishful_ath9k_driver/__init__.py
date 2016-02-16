60f1bce54cbd7eeb98e61f00505524a0ab94dff5
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-16 15:06:41 +0100

    object serialization available

--- wishful_ath9k_driver/__init__.py
+++ wishful_ath9k_driver/__init__.py
@@ -1,2 +1,2 @@
-
 from ath9k_driver import *
+from edca import *
--- wishful_ath9k_driver/ath9k_driver.py
+++ wishful_ath9k_driver/ath9k_driver.py
@@ -1,6 +1,7 @@
 import logging
 import wishful_module
 import wishful_upis as upis
+import edca #<----!!!!! Important to include it here; otherwise cannot be pickled!!!!
 
 
 __author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
@@ -15,24 +16,45 @@ class Ath9kDriver(wishful_module.WishfulModule):
         super(Ath9kDriver, self).__init__(agentPort)
         self.log = logging.getLogger('ath9k_module.main')
         self.interface = "wlan0"
+        self.channel = 1
+        self.power = 1
 
 
     @wishful_module.bind_function(upis.radio.set_channel)
     def set_channel(self, channel):
         self.log.debug("ATH9K sets channel: {} on interface: {}".format(channel, self.interface))
-
+        self.channel = channel
         return "SET_CHANNEL_OK"
 
 
     @wishful_module.bind_function(upis.radio.get_channel)
-    def get_channel(self, mockup):
+    def get_channel(self):
         self.log.debug("Gets channel of interface: {}".format(self.interface))
-
-        return "Channel:3"
+        return "CHANNEL_{}".format(self.channel)
 
 
     @wishful_module.bind_function(upis.radio.set_power)
     def set_power(self, power):
         self.log.debug("ATH9K sets power: {} on interface: {}".format(power, self.interface))
+        self.power = power
+        return "SET_POWER_OK_value_{}".format(power)
 
-        return "SET_POWER_OK"
+
+    @wishful_module.bind_function(upis.radio.get_power)
+    def get_power(self):
+        self.log.debug("ATH9K gets power on interface: {}".format(self.interface))
+        return "POWER_{}".format(self.power)
+
+
+    @wishful_module.bind_function(upis.radio.setEdcaParameters)
+    def setEdcaParameters(self, queueId, queueParams):
+        self.log.debug("ATH9K sets EDCA parameters for queue: {} on interface: {}".format(queueId, self.interface))
+
+        print "Setting EDCA parameters for queue: {}".format(queueId)
+        print "AIFS: {}".format(queueParams.getAifs())
+        print "CwMin: {}".format(queueParams.getCwMin())
+        print "CwMax: {}".format(queueParams.getCwMax())
+        print "TxOp: {}".format(queueParams.getTxOp())
+
+        return "EDCA_OK"
+
